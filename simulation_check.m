c09e9a4a47d6fc06f5193cd1352b945c0b827f14
mohitk74 <163093956+mohitk74@users.noreply.github.com>
2024-06-11 17:43:55 +0530

    Delete simulation_check.m

--- simulation_check.m
+++
@@ -1,8 +0,0 @@
-modelName = 'and_gate_model.slx';
-sim(modelName);
-simOut = sim(modelName, 'ReturnWorkspaceOutputs', 'on');
-if ~isempty(simOut)
-    disp('Model has been simulated');
-else
-    disp('Model has not been simulated');
-end
